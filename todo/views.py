be9604a18c10f3766cf307ab3c39f4b109608959
toki <takaya.4208@gmail.com>
2021-08-03 23:39:59 +0900

    Display up to "Hello World"

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Todo
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+  latest_todo_list = Todo.objects.order_by('-updated_at')[:5]
+  context={
+    'latest_todo_list' : latest_todo_list
+  }
+  return render(request, 'todo/index.html', context)
