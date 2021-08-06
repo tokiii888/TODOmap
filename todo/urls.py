9fb5228bff32a4199c42100beab7aee8ff143073
toki <takaya.4208@gmail.com>
2021-08-06 15:35:33 +0900

    Change to view using generic

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'todo'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.Index.as_view(), name='index'),
+    path('detail/<int:pk>/', views.Detail.as_view(), name='detail'),
+    path('create/', views.Create.as_view(), name='create'),
+    path('update/<int:pk>/', views.Update.as_view(), name='update'),
+    path('delete/<int:pk>/', views.Delete.as_view(), name='delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
+from django import forms
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from .models import Todo
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
@@ -10,3 +12,48 @@ def index(request):
     'latest_todo_list' : latest_todo_list
   }
   return render(request, 'todo/index.html', context)
+
+class Form(forms.ModelForm):
+  class Meta:
+    model = Todo
+    exclude = ('created_at','updated_at',)  #入力項目から作成日時、更新日時を除外
+
+# 一覧表示
+class Index(ListView):
+  template_name = "index.html"
+  model = Todo
+  paginate_by = 10 # ページネーションが5ページまで
+
+# 詳細表示
+class Detail(DetailView):
+  template_name = "detail.html"
+  model = Todo
+
+# 新規作成
+class Create(CreateView):
+  template_name = "form.html"
+  model = Todo
+  form_class = Form
+  success_url = reverse_lazy('todo:index')
+
+# 更新
+class Update(UpdateView):
+  template_name = "form.html"
+  model = Todo
+  form_class = Form
+  success_url = reverse_lazy('todo:index')
+
+# 削除
+class Delete(DeleteView):
+  """
+  Todoの削除
+  【デフォルトの動作】
+  getリクエスト・・・確認ページへ遷移
+  postリクエスト・・・削除を実行
+  【補足】
+  レコードを削除せず、有効フラグを消す(論理削除)場合は
+  deleteをオーバーライドしてその中に処理を書く
+  """
+  template_name = "delete.html"
+  model = Todo
+  success_url = reverse_lazy('todo:index')
