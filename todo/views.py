bc405e7db223fce3c23eca19975d447f59936a3d
toki <takaya.4208@gmail.com>
2021-08-10 23:20:21 +0900

    Implemented even a todo application

--- todo/views.py
+++ todo/views.py
@@ -6,39 +6,36 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 
 # Create your views here.
 
-def index(request):
-  latest_todo_list = Todo.objects.order_by('-updated_at')[:5]
-  context={
-    'latest_todo_list' : latest_todo_list
-  }
-  return render(request, 'todo/index.html', context)
-
 class Form(forms.ModelForm):
+  #todo = forms.CharField(widget=forms.Textarea, label='')
   class Meta:
     model = Todo
     exclude = ('created_at','updated_at',)  #入力項目から作成日時、更新日時を除外
-
+    widgets = {
+      #'todo': forms.TextInput(attrs={'autocomplete': 'off'}),
+      'todo': forms.Textarea(attrs={'autocomplete': 'off', 'cols': 70, 'rows': 10}, ),
+    }
 # 一覧表示
 class Index(ListView):
-  template_name = "index.html"
+  template_name = "todo/index.html"
   model = Todo
   paginate_by = 10 # ページネーションが5ページまで
 
 # 詳細表示
 class Detail(DetailView):
-  template_name = "detail.html"
+  template_name = "todo/detail.html"
   model = Todo
 
 # 新規作成
 class Create(CreateView):
-  template_name = "form.html"
+  template_name = "todo/form.html"
   model = Todo
   form_class = Form
   success_url = reverse_lazy('todo:index')
 
 # 更新
 class Update(UpdateView):
-  template_name = "form.html"
+  template_name = "todo/form.html"
   model = Todo
   form_class = Form
   success_url = reverse_lazy('todo:index')
@@ -54,6 +51,6 @@ class Delete(DeleteView):
   レコードを削除せず、有効フラグを消す(論理削除)場合は
   deleteをオーバーライドしてその中に処理を書く
   """
-  template_name = "delete.html"
+  template_name = "todo/delete.html"
   model = Todo
   success_url = reverse_lazy('todo:index')
