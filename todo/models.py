23eab6213a9aae58d24331212222eb271679f15e
toki <takaya.4208@gmail.com>
2021-08-19 10:45:57 +0900

    Add latitude and longitude to model.

--- todo/models.py
+++ todo/models.py
@@ -7,6 +7,8 @@ class Todo(models.Model):
   created_at = models.DateTimeField('作成日時', auto_now_add=True)
   updated_at = models.DateTimeField('更新日時',auto_now=True)
   adress = models.CharField('住所', max_length=100, blank=False)
+  lat = models.DecimalField('緯度', max_digits=9, decimal_places=6, default=0)
+  lng = models.DecimalField('経度', max_digits=9, decimal_places=6, default=0)
 
   def __str__(self):
     return self.todo
