f88a9950228f22bdda827089e8d10f5200e87b60
toki <takaya.4208@gmail.com>
2021-08-24 15:15:04 +0900

    Sort by distance

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,11 @@ from django.urls import reverse_lazy
 from .models import Todo
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 import requests
+import geocoder
+from django.db.models.expressions import RawSQL
+import math
+from django.db.backends.signals import connection_created
+from django.dispatch import receiver
 
 #googleapikey = '取得したGoogleのAPIキー'
 
@@ -19,21 +24,44 @@ class Form(forms.ModelForm):
       'adress': forms.Textarea(attrs={'autocomplete': 'off', 'cols': 60, 'rows': 1},),
     }
 
+@receiver(connection_created)
+def extend_sqlite(connection=None, **kwargs):
+    if connection.vendor == "sqlite":
+        # sqlite doesn't natively support math functions, so add them
+        cf = connection.connection.create_function
+        cf('acos', 1, math.acos)
+        cf('cos', 1, math.cos)
+        cf('radians', 1, math.radians)
+        cf('sin', 1, math.sin)
+
 # 一覧表示
 class Index(ListView):
   template_name = "todo/index.html"
   model = Todo
   paginate_by = 10 # ページネーションが5ページまで
   def get_queryset(self):
-    print("接続!")
+    def get_locations_nearby_coords(latitude, longitude):
+      """
+      Return objects sorted by distance to specified coordinates
+      which distance is less than max_distance given in kilometers
+      """
+      # Great circle distance formula
+      gcd_formula = "6371 * acos(\
+      cos(radians(%s)) * cos(radians(lat)) \
+      * cos(radians(lng) - radians(%s)) + \
+      sin(radians(%s)) * sin(radians(lat)) \
+      )"
+      distance_raw_sql = RawSQL(
+          gcd_formula,
+          (latitude, longitude, latitude)
+      )
+      qs = Todo.objects.all() \
+      .annotate(distance=distance_raw_sql)\
+      .order_by('distance')
+      return qs
     geo_request_url = 'https://get.geojs.io/v1/ip/geo.json'
     geo_data = requests.get(geo_request_url).json()
-    print(geo_data['latitude'])
-    print(geo_data['longitude'])
-    ref_location = Point(geo_data['latitude'], geo_data['longitude'], srid=4326)
-    return Todo.objects.annotate(
-      distance=GeometryDistance('location', ref_location)
-      ).order_by('distance')
+    return get_locations_nearby_coords(float(geo_data['latitude']), float(geo_data['longitude']))
 
 # 詳細表示
 class Detail(DetailView):
